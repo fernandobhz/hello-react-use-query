0f1e6f2b4cd7199a80650bbf757c6e1bf9d27c99
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-10-27 20:17:12 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useLocation, BrowserRouter, Link, useParams } from "react-router-dom";
+
+const MyComponent = () => {
+  const location = useLocation();
+  return <div>Path is: {location.pathname}</div>;
+};
+
+function Content(props) {
+  const location = useLocation();
+  const { search } = location;
+  const urlParams = new URLSearchParams(search);
+  const orderId = urlParams.get("id");
+
+  const params = useParams();
+
+  return (
+    <div className="App">
+      <a href="/order?id=123">Order 123</a>&nbsp;-&nbsp;
+      <Link to={{ pathname: "/order", search: "?id=456" }}>Order 456</Link>&nbsp;-&nbsp;
+      <Link to={{ pathname: "/order", search: "?id=789" }}>Order 789</Link>
+      <hr />
+      Location: {JSON.stringify(location)}<br />
+      Search: {search}<br />
+      Url Params: {JSON.stringify(urlParams)}<br />
+      Order number: {orderId}<br />
+      Props: {JSON.stringify(props)}<br/>
+      Params: {JSON.stringify(params)}<br />
+
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <MyComponent />
+      <Content />
+    </BrowserRouter>
   );
 }
 
